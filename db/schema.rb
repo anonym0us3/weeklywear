580f4d10ef3267f848b1ba7cb5087505cc077161
anonym0us3 <nathan.shayevich@gmail.com>
2016-02-21 14:23:35 -0800

    added basic user model

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160219210434) do
+ActiveRecord::Schema.define(version: 20160221222156) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "users", force: :cascade do |t|
-    t.string   "first_name"
     t.string   "username"
     t.string   "email"
     t.string   "password_digest"
