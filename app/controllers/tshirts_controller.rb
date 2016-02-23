52e014e2cd588fe04efec46eec1072f1fcdee07e
anonym0us3 <nathan.shayevich@gmail.com>
2016-02-23 12:11:17 -0800

    added edit, update, destroy methods for tshirts + DRY'd the code

--- app/controllers/tshirts_controller.rb
+++ app/controllers/tshirts_controller.rb
@@ -5,21 +5,39 @@ class TshirtsController < ApplicationController
     @tshirts = Tshirt.all
   end
 
+  # Show signup form for adding new t-shirt
   def new
     @tshirt = Tshirt.new
   end
 
-
+  # Process form data & create new t-shirt; login & redirect to user's t-shirt management page
   def create
     @tshirt = Tshirt.new(tshirt_params)
     @tshirt.save
 
-    redirect_to tshirts_path
+    redirect_to user_tshirts_path(current_user)
   end
 
-  # Display 1 specific tshirt, by ID
   def show
-    @tshirt = Tshirt.find_by_id(params[:id])
+    @tshirt = find_tshirt
+  end
+
+  def edit
+    @tshirt = find_tshirt
+  end
+
+  def update
+    tshirt = find_tshirt
+    tshirt.update_attributes(tshirt_params)
+
+    redirect_to user_tshirts_path(current_user)
+  end
+
+  def destroy
+    @tshirt = find_tshirt
+    @tshirt.destroy
+
+    redirect_to user_tshirts_path(current_user)
   end
 
   def user_tshirts
@@ -27,11 +45,17 @@ class TshirtsController < ApplicationController
     @tshirts = Tshirt.all
   end
 
+
   private
 
   # Whitelist of permitted form data
   def tshirt_params
     params.require(:tshirt).permit(:name, :image).merge(:user_id => current_user.id)
+  end
+
+  # Attempting DRYness for show, edit, update, destroy methods
+  def find_tshirt
+    Tshirt.find_by_id(params[:id])
   end
 
 end
