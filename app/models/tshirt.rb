80856abdc557c43735760776b4575042211b5a1b
anonym0us3 <nathan.shayevich@gmail.com>
2016-02-23 12:12:21 -0800

    default sort-order for tshirts to display by descending created_at date/time

--- app/models/tshirt.rb
+++ app/models/tshirt.rb
@@ -6,4 +6,7 @@ class Tshirt < ActiveRecord::Base
   has_attached_file :image, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/missingtee.jpg"
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
 
+  # Sets default sort order to be Descending by created_at time/date (aka, newest-to-oldest)
+  default_scope { order("created_at DESC") }
+
 end
