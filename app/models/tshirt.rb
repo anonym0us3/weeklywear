b62b1c29f96e4fa8841f842f6905a45b1a30e1c3
anonym0us3 <nathan.shayevich@gmail.com>
2016-02-23 16:50:09 -0800

    added relationship between users & tshirts --> votes

--- app/models/tshirt.rb
+++ app/models/tshirt.rb
@@ -1,7 +1,7 @@
 class Tshirt < ActiveRecord::Base
 
   belongs_to :user
-  has_many :votes
+  has_many :votes, dependent: :destroy
 
   has_attached_file :image, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/missingtee.jpg"
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
