6d0e00f32ce84e5fd6960fea61e9d75ebedd89a4
anonym0us3 <nathan.shayevich@gmail.com>
2016-02-24 16:35:17 -0800

    added validations to user sign-in page

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,27 @@
 class User < ActiveRecord::Base
   has_secure_password
 
+  before_save { self.email = email.downcase }
+
   has_many :tshirts, dependent: :destroy
   has_many :votes, dependent: :destroy
   
   has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/missing.png"
   validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\Z/
+
+  validates :username, :email, :password,
+    presence: true,
+    length: { maximum: 255 }
+
+  VALID_EMAIL_REGEX = /\A[\w+\-.\+]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email,
+    format: { with: VALID_EMAIL_REGEX },
+    uniqueness: { case_sensitive: false }
+
+  VALID_PASSWORD_REGEX = /[a-zA-Z0-9]+/
+  validates :password,
+    length: { minimum: 8 },
+    format: { with: VALID_PASSWORD_REGEX }
 
   def self.confirm(params)
     @user = User.find_by({email: params[:email]})
